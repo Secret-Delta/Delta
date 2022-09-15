21d7dba81ee726f0ed9298dee29dd782821f4d00
it21151392 <87414583+it21151392@users.noreply.github.com>
2022-09-16 03:03:27 +0530

    create calendar view on Calendar

--- app/src/main/java/com/SecretDelta/delta/Fragments/CalendarFragment.java
+++ app/src/main/java/com/SecretDelta/delta/Fragments/CalendarFragment.java
@@ -7,17 +7,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.SecretDelta.delta.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class CalendarFragment extends Fragment {
 
+    TextView currentDate;
+    Calendar calendar;
+    SimpleDateFormat dateFormat;
+    String date;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        calendar = Calendar.getInstance();
+        currentDate = view.findViewById(R.id.displayCurrentDate);
+        dateFormat = new SimpleDateFormat("EEEE, d MMM, yyyy");
+        date = dateFormat.format(calendar.getTime());
+        currentDate.setText(date);
+
+        return view;
     }
 }
