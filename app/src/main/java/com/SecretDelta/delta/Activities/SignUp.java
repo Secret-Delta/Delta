0a7aea1f65d38fe788e8f76e8bafb41ced48c169
it21151392 <87414583+it21151392@users.noreply.github.com>
2022-09-18 01:31:19 +0530

    create FAB, Theme change and New Event Activity

---
+++ app/src/main/java/com/SecretDelta/delta/Activities/SignUp.java
@@ -0,0 +1,16 @@
+package com.SecretDelta.delta.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.SecretDelta.delta.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
