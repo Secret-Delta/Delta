dd2460a2c486779b14c40576a49cc8713ece144b
it21151392 <87414583+it21151392@users.noreply.github.com>
2022-09-18 17:05:59 +0530

    create & finished profile

--- app/src/main/java/com/SecretDelta/delta/Activities/SignIn.java
+++ app/src/main/java/com/SecretDelta/delta/Activities/SignIn.java
@@ -2,15 +2,25 @@ package com.SecretDelta.delta.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.SecretDelta.delta.R;
 
 public class SignIn extends AppCompatActivity {
-
+Button btnNext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        btnNext = (Button) findViewById(R.id.btnSignup);
+    }
+
+    public void openAccount(View view) {
+        Intent openProfile = new Intent(this, UserProfile.class);
+        startActivity(openProfile);
     }
 }
---
+++ app/src/main/java/com/SecretDelta/delta/Activities/UserProfile.java
@@ -0,0 +1,16 @@
+package com.SecretDelta.delta.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.SecretDelta.delta.R;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
