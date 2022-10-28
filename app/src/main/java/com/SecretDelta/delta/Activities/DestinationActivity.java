f603153e28b258f641eb809a4ed80b148183e2d4
Navod <54998118+navodveduth@users.noreply.github.com>
2022-10-29 01:14:22 +0530

    add task notification

---
+++ app/src/main/java/com/SecretDelta/delta/Activities/DestinationActivity.java
@@ -0,0 +1,18 @@
+package com.SecretDelta.delta.Activities;
+
+
+import android.os.Bundle;
+
+import com.SecretDelta.delta.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class DestinationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
