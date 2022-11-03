e00b0c5d49667d1647fd10b26ef0b372cb7efbab
it21151392 <87414583+it21151392@users.noreply.github.com>
2022-11-03 21:00:14 +0530

    registration and login

--- app/src/main/java/com/SecretDelta/delta/Activities/SignIn.java
+++ app/src/main/java/com/SecretDelta/delta/Activities/SignIn.java
@@ -1,26 +1,88 @@
 package com.SecretDelta.delta.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.SecretDelta.delta.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignIn extends AppCompatActivity {
-Button btnNext;
+
+    private FirebaseAuth auth;
+    private EditText loginEmail, loginPassword;
+    private TextView signupRedirect;
+    private Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        btnNext = (Button) findViewById(R.id.btnSignup);
+        auth = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.email_address);
+        loginPassword = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.btnSignup);
+        signupRedirect = findViewById(R.id.sign_up_redirect);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = loginEmail.getText().toString();
+                String pass = loginPassword.getText().toString();
+
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if (!pass.isEmpty()) {
+                        auth.signInWithEmailAndPassword(email, pass)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(SignIn.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(SignIn.this, MainActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(SignIn.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        loginPassword.setError("Password cannot be empty");
+                    }
+                } else if(email.isEmpty()) {
+                    loginEmail.setError("Email cannot be empty");
+                } else {
+                    loginEmail.setError("Please enter valid email");
+                }
+            }
+        });
+
+
+        signupRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignIn.this,SignUp.class));
+            }
+        });
+        ;
     }
 
-    public void openAccount(View view) {
-        Intent openProfile = new Intent(this, UserProfile.class);
-        startActivity(openProfile);
-    }
+
+
+//    public void openAccount(View view) {
+//        Intent openProfile = new Intent(this, UserProfile.class);
+//        startActivity(openProfile);
+//    }
 }
--- app/src/main/java/com/SecretDelta/delta/Activities/SignUp.java
+++ app/src/main/java/com/SecretDelta/delta/Activities/SignUp.java
@@ -1,27 +1,75 @@
 package com.SecretDelta.delta.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.SecretDelta.delta.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
 
-    Button btnSignup;
+    private FirebaseAuth auth;
+    private EditText signUpEmail, signUpPwd, signUpName;
+    private Button signUpBtn;
+    private TextView loginRedirect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        btnSignup = (Button) findViewById(R.id.btnSignup);
+
+        auth = FirebaseAuth.getInstance();
+        signUpEmail = findViewById(R.id.editEmailAddress);
+        signUpPwd = findViewById(R.id.editName);
+        signUpName = findViewById(R.id.editName);
+        signUpBtn = findViewById(R.id.btnSignup);
+        loginRedirect = findViewById(R.id.loginRedirect);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = signUpEmail.getText().toString().trim();
+                String pass = signUpPwd.getText().toString().trim();
+
+                if (user.isEmpty()){
+                    signUpEmail.setError("Email cannot be empty");
+                }
+                if (pass.isEmpty()){
+                    signUpPwd.setError("Password cannot be empty");
+                } else{
+                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SignUp.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignUp.this, SignIn.class));
+                            } else {
+                                Toast.makeText(SignUp.this, "SignUp Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        loginRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUp.this, SignIn.class));
+            }
+        });
+
     }
 
-    public void openLogin(View view) {
-        Intent i1 = new Intent(this, SignIn.class);
-        startActivity(i1);
-    }
 }
